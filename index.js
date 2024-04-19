e79d3342166a9640b36c3e57c2a556037e89b8a7
An Nguyen <vanan6b@gmail.com>
2024-04-20 02:30:35 +0700

    update code

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 
 dotenv.config();
+
+app.use(cors({origin: '*'}));
+
 
 require('./src/routes/router.module.js')(app);
 
@@ -13,4 +18,4 @@ let port = process.env.PORT || 8080
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+}) 
