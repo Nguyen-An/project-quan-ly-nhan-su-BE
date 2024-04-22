9b4c24b593c34d97af84e4aa6bc77d778d7f46fd
monly <vanan6b@gmail.com>
2024-04-22 15:12:35 +0700

    update code

--- src/models/employee.model.js
+++ src/models/employee.model.js
@@ -4,7 +4,7 @@ const Emnployee = (emnployee) => {
 
 }
 
-Emnployee.getList = (keywords) => {
+Emnployee.getList = async (keywords) => {
     if(!keywords) keywords = "";
     keywords = keywords.trim();
     keywords = `%${keywords}%`
@@ -16,25 +16,49 @@ Emnployee.getList = (keywords) => {
     })
 }
 
+// Emnployee.postEmployee =  (emp) => {
+//     return new Promise((resolve, reject) => {
+//         db.query("INSERT INTO employee(userName, userCode, gender, dateOfBirth, phone, address, email, identityCard, status, position, laborContract, salary, dateEnd, dateStart) VALUES (?, ?, ?, ?, ?, ? , ? , ? , ? , ? , ? , ? , ? , ?);",
+//             [emp.userName, emp.userCode, emp.gender, emp.dateOfBirth, emp.phone, emp.address, emp.email, emp.identityCard, emp.status, emp.position, emp.laborContract, emp.salary, emp.dateEnd, emp.dateStart], (err, result) => {
+//                 if (err) {
+//                     console.log('err: ', err);
+//                     reject(null)
+//                 }
+//                 resolve(result)
+//             })
+//     })
+// }
+
 Emnployee.postEmployee = (emp) => {
-    return new Promise((resolve, reject) => {
-        db.query("INSERT INTO employee(userName, userCode, gender, dateOfBirth, phone, address, email, identityCard, status, position, laborContract, salary, dateEnd, dateStart) VALUES (?, ?, ?, ?, ?, ? , ? , ? , ? , ? , ? , ? , ? , ?);",
-            [emp.userName, emp.userCode, emp.gender, emp.dateOfBirth, emp.phone, emp.address, emp.email, emp.identityCard, emp.status, emp.position, emp.laborContract, emp.salary, emp.dateEnd, emp.dateStart], (err, result) => {
-                if (err) {
-                    console.log('err: ', err);
-                    reject(null)
-                }
-                resolve(result)
-            })
+    return new Promise((resolve, reject) => { 
+        // Query to get the maximum id
+        db.query("SELECT MAX(employeeId) AS maxId FROM employee", (err, rows) => {
+            if (err) {
+                console.log('err: ', err);
+                reject(null);
+            } else {
+                // Step 2: Generate new employee code
+                let idMax = rows[0].maxId || 0;
+                let newEmployeeCode = "NV" + (idMax + 1);
+                db.query("INSERT INTO employee(userName, userCode, gender, dateOfBirth, phone, address, email, identityCard, status, position, laborContract, salary, dateEnd, dateStart) VALUES (?, ?, ?, ?, ?, ? , ? , ? , ? , ? , ? , ? , ? , ?);",
+                    [emp.userName, newEmployeeCode, emp.gender, emp.dateOfBirth, emp.phone, emp.address, emp.email, emp.identityCard, emp.status, emp.position, emp.laborContract, emp.salary, emp.dateEnd, emp.dateStart], (err, result) => {
+                    if (err) {
+                        console.log('err: ', err);
+                        reject(null)
+                    }
+                    resolve(result)
+                })
+            }
+        })
     })
 }
 
 Emnployee.updateEmployee = (employeeId, emp) => {
     return new Promise((resolve, reject) => {
         db.query(`UPDATE employee e
-        SET e.userName = ?, e.userCode = ?, e.gender = ?, e.dateOfBirth = ?, e.phone = ?, e.address = ?, e.email = ?, e.identityCard = ?, e.status = ?, e.position = ?, e.laborContract = ?, e.salary = ?, e.dateEnd = ?, e.dateStart = ?
+        SET e.userName = ?, e.gender = ?, e.dateOfBirth = ?, e.phone = ?, e.address = ?, e.email = ?, e.identityCard = ?, e.status = ?, e.position = ?, e.laborContract = ?, e.salary = ?, e.dateEnd = ?, e.dateStart = ?
         WHERE e.employeeId = ?;`,
-            [emp.userName, emp.userCode, emp.gender, emp.dateOfBirth, emp.phone, emp.address, emp.email, emp.identityCard, emp.status, emp.position, emp.laborContract, emp.salary, emp.dateEnd, emp.dateStart, employeeId], (err, result) => {
+            [emp.userName, emp.gender, emp.dateOfBirth, emp.phone, emp.address, emp.email, emp.identityCard, emp.status, emp.position, emp.laborContract, emp.salary, emp.dateEnd, emp.dateStart, employeeId], (err, result) => {
                 if (err) {
                     console.log('err: ', err);
                     reject(null)
